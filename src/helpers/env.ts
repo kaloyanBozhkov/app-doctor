95897ad692be6c31d884a01b87261ac7af42e61c
Kaloyan Bozhkov <your-email@example.com>
2025-11-27 18:33:15 +0700

    .

--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -1,13 +1,11 @@
 import { z } from "zod";
 
-export const env = require("@t3-oss/env-core").createEnv({
-  server: {
-    NODE_ENV: z
-      .enum(["development", "production", "test"])
-      .default("development"),
-    PORT: z.string().transform(Number).default("3000"),
-    X_API_KEY: z.string().min(1),
-  },
-  runtimeEnv: process.env,
-  emptyStringAsUndefined: true,
+const envSchema = z.object({
+  NODE_ENV: z
+    .enum(["development", "production", "test"])
+    .default("development"),
+  PORT: z.string().transform(Number).default("3000"),
+  X_API_KEY: z.string().min(1),
 });
+
+export const env = envSchema.parse(process.env);
