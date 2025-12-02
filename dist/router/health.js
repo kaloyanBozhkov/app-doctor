ef7fc13ddb27b7ae532dfbc4f67736f451324878
Kaloyan Bozhkov <your-email@example.com>
2025-12-03 02:01:17 +0700

    .

--- dist/router/health.js
+++ dist/router/health.js
@@ -11,7 +11,7 @@ const PROJECTS = {
         { "x-api-key": env_1.env.TG_BOT_KEY },
     ],
     linkbase: ["https://linkbase.kaloyanbozhkov.com/health", {}],
-    "linkbase-db": ["https://linkbase.kaloyanbozhkov.com/health-db", {}],
+    "linkbase/db": ["https://linkbase.kaloyanbozhkov.com/health-db", {}],
     naslqpo: ["https://naslqpo.com/api/health", {}],
     "naslqpo-db": ["https://naslqpo.com/api/health/db", {}],
     znp: ["https://znp.show/api/health/db", {}],
